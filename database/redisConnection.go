c35914f0957cdedf9328de90b24c994f3f30d14e
Tony Zhuo <tony860725@gmail.com>
2022-05-02 21:58:31 +0800

    feat: add redis connection setting, includ docker

--- database/redisConnection.go
+++ database/redisConnection.go
@@ -1,1 +1,19 @@
 package database
+
+import (
+	"github.com/go-redis/redis/v8"
+	_ "github.com/joho/godotenv/autoload"
+	"os"
+)
+
+func RedisConnect() *redis.Client {
+	Host := os.Getenv("REDIS_HOST")
+	Port := os.Getenv("REDIS_PORT")
+	Password := os.Getenv("REDIS_PASSWORD")
+
+	return redis.NewClient(&redis.Options{
+		Addr:     Host + ":" + Port,
+		Password: Password, // no password set
+		DB:       0,        // use default DB
+	})
+}
